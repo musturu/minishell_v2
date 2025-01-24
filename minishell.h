a54e5834884ea089d1938d0f9fca83c799d3164f
musturu <lorenzo.moriconi@ko2.it>
2025-01-24 19:46:37 +0100

    update

--- minishell.h
+++ minishell.h
@@ -1,27 +1,29 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <string.h>
-# include <fcntl.h>
-# include <sys/wait.h>
-# include <signal.h>
-# include <sys/stat.h>
-# include <dirent.h>
-# include <sys/ioctl.h>
-# include <termios.h>
+#include <stdio.h>     // Per printf, perror
+#include <stdlib.h>    // Per malloc, free, exit, getenv
+#include <unistd.h>    // Per write, access, read, close, fork, execve, dup, dup2, pipe, getcwd, chdir, isatty, ttyname, ttyslot
+#include <string.h>    // Per strerror
+#include <fcntl.h>     // Per open
+#include <sys/wait.h>  // Per wait, waitpid, wait3, wait4
+#include <signal.h>    // Per signal, sigaction, sigemptyset, sigaddset, kill
+#include <sys/stat.h>  // Per stat, lstat, fstat
+#include <dirent.h>    // Per opendir, readdir, closedir
+#include <sys/ioctl.h> // Per ioctl
+#include <termios.h>   // Per tcsetattr, tcgetattr
 
-# include "src/builtin/builtin.h"
-# include <readline/readline.h>
-# include <readline/history.h>
-# include "libft/libft.h"
+#include "src/builtin/builtin.h"
 
-# define PROMPT "MINISHELL$>"
-# define NEEDFORK(arg) (is_builtin(arg) != 1)
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft/libft.h"
+// Definizioni di prototipi di funzione, se necessario
+// ...
+#define PROMPT "MINISHELL$>"
+#define NEEDFORK(arg) is_builtin(arg) != 1
 
-typedef enum e_TokenType
+typedef enum
 {
     TOKEN_WORD,
     TOKEN_PIPE,
@@ -30,60 +32,67 @@ typedef enum e_TokenType
     TOKEN_REDIR_APPEND,
     TOKEN_REDIR_PRE,
     TOKEN_EOF,
-} t_TokenType;
+} e_TokenType;
 
 typedef struct s_token
 {
-    t_TokenType type;
+    e_TokenType type;
     char *value;
 } t_token;
 
 typedef struct s_cmd
 {
-    char *cmd;
-    t_list *args;
-    char *inpath;
-    char *outpath;
-    char **argv;
-    int infd;
-    int outfd;
-    t_TokenType inconnect;
-    t_TokenType outconnect;
+    char *cmd;           // command name / path
+    t_list *args;        // list of arguments
+    char *inpath;        // file name of INPUT
+    char *outpath;       // file name of OUTPUT
+    char **argv;         // arguments matrix (necessary for execve)
+    int infd;            // input fd
+    int outfd;           // output fd
+    e_TokenType inconnect; // 6 possibilities in bash = & | ; && || EOF
+    e_TokenType outconnect;
 } t_command;
 
 extern int g_status;
 
+/*TOKENIZER*/
 int space_until_next(char *str);
 t_list *tokenize(char *str, t_list **list);
-int ft_istokenquotes(char c);
-int append_token(char **str, t_list **lst);
+int	ft_istokenquotes(char c);
+int	append_token(char **str, t_list **lst);
 
+/*EXPANDER*/
 t_list *expand(t_list **list, char **env);
 char *expand_token_value(const char *value);
-char *expand_token(char *str, char **env);
+char	*expand_token(char *str, char **env);
 
+/*debug*/
 void print_tokens(t_list *list);
 void print_parse(t_list *list);
 
-int append_cmd(t_list **tokens, t_list **parsed_list);
-t_TokenType get_command_inconnect(t_list **tokens, int isfirst);
-char is_redirection(t_TokenType type);
+/*parser*/
+int	append_cmd(t_list	**tokens, t_list **parsed_list);
+e_TokenType get_command_inconnect(t_list **tokens, int isfirst);
+char is_redirection(e_TokenType type);
 char is_break(t_list *tokens);
-char is_string(t_TokenType type);
+char is_string(e_TokenType type);
 char is_after_break(t_list *tokens);
 
+/*execute*/
 int exec_builtin(char *cmd, char **argv, char ***env, int *ret);
 int execute(t_list **parsed_list, char ***env);
-int heredoc(t_command *command, char **env);
-int pipes(t_list **lst);
+int	heredoc(t_command *command, char **env);
+int	pipes(t_list **lst);
 
+/*memory*/
 void free_command(void *cmd);
 void free_token(void *tkn);
 void free_matrix(char **mat);
 
+/*utils*/
 int blank_check(char *str);
-char **ft_matdup(char **mat);
-char *get_env(char **env, char *name);
+char    **ft_matdup(char **mat);
+char	*get_env(char **env, char *name);
 char **listomap(char *cmd, t_list *list);
 int str_to_env_index(char **env, char *name);
 int ft_istokenchar(char c);
@@ -91,9 +100,10 @@ char is_builtin(char *cmd);
 void ft_lst_remove_node(t_list **lst, t_list *node, void (*del)(void *));
 int ft_isspace(char c);
 int ft_contains_dollar(const char *str);
-void close_fds(t_list *lst);
+void	close_fds(t_list *lst);
 
-void ft_signals(void);
-void handlec_heredoc(int sig);
+/*signals*/
+void	ft_signals(void);
+void	handlec_heredoc(int sig);
 
 #endif
