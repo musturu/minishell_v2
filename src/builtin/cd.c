afed184c88b6880307c1eb128fc99e88d35d606f
Lorenzo Moriconi <lmoricon@e3r4p6.42roma.it>
2025-01-24 20:32:04 +0100

    update

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -1,30 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cd.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmoricon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 20:30:26 by lmoricon          #+#    #+#             */
+/*   Updated: 2025/01/24 20:31:47 by lmoricon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "builtin.h"
 #include <dirent.h>
 
-int cd(char **argv)
+int	cd(char **argv)
 {
-	int i;
+	int	i;
+
 	i = 0;
-	(void)env;
 	while (argv[i])
 		i++;
 	if (i == 1)
-	{
 		return (chdir(getenv("HOME")));
-	}
 	if (i != 2)
-	{
-		printf("Error: Wrong Argument count :)\n");
-		return -1;
-	}
+		return (printf("Error: Wrong Argument count :)\n"));
 	if (opendir(argv[1]) == NULL)
-	{
-		printf("Error cd: no such file or directory :)\n");
-		return 12;	
-
-	}
+		return (printf("Error cd: no such file or directory :)\n"));
 	return (chdir(argv[1]));
 }
