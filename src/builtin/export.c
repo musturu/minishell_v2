3ea08b539943f4045d00359dd313da537f92ef65
merlina-wizard <merlini.genshin@gmail.com>
2025-01-27 12:17:25 +0100

    update

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -18,14 +18,17 @@ int	already_present(char *str, char **env)
 	int		len;
 	char	*eq;
 
-	i = -1;
+	i = 0;
 	eq = ft_strchr(str, '=');
-	len = eq ? (eq - str) : ft_strlen(str);
-	while (env[++i])
+	if (eq)
+		len = eq - str;
+	else
+		len = ft_strlen(str);
+	while (env[i])
 	{
-		if (!ft_strncmp(str, env[i], len)
-			&& (env[i][len] == '=' || env[i][len] == '\0'))
+		if (!ft_strncmp(str, env[i], len) && (env[i][len] == '=' || env[i][len] == '\0'))
 			return (i);
+		i++;
 	}
 	return (-1);
 }
@@ -39,18 +42,26 @@ void	matrixdup(char ***dst, char ***src)
 	if (!src || !*src)
 	{
 		*dst = NULL;
-		return ;
+		return;
 	}
 	while ((*src)[len])
 		len++;
 	*dst = (char **)malloc((len + 1) * sizeof(char *));
 	if (!*dst)
-		return ;
-	for (i = 0; i < len; i++)
+		return;
+	i = 0;
+	while (i < len)
+	{
 		(*dst)[i] = ft_strdup((*src)[i]);
+		i++;
+	}
 	(*dst)[len] = NULL;
-	for (i = 0; i < len; i++)
+	i = 0;
+	while (i < len)
+	{
 		free((*src)[i]);
+		i++;
+	}
 	free(*src);
 	*src = *dst;
 }
@@ -60,37 +71,46 @@ void	update_exp(char *str, char ***env)
 	int	i;
 
 	i = already_present(str, *env);
-	if (i == -1)
-		return ;
-	free((*env)[i]);
-	(*env)[i] = ft_strdup(str);
+	if (i != -1)
+	{
+		free((*env)[i]);
+		(*env)[i] = ft_strdup(str);
+	}
 }
 
 void	insert_exp(char *str, char ***env)
 {
 	char	**new_env;
 	int		i;
+	int		j;
 
 	i = 0;
 	while ((*env)[i])
 		i++;
 	new_env = ft_calloc(i + 2, sizeof(char *));
 	if (!new_env)
-		return ;
-	for (int j = 0; j < i; j++)
+		return;
+	j = 0;
+	while (j < i)
+	{
 		new_env[j] = ft_strdup((*env)[j]);
+		j++;
+	}
 	if (ft_strchr(str, '=') != NULL)
 	{
 		new_env[i] = ft_strdup(str);
-		new_env[i + 1] = NULL;
 	}
 	else
 	{
 		new_env[i] = ft_strjoin(str, "=''");
-		new_env[i + 1] = NULL;
 	}
-	for (int j = 0; j < i; j++)
+	new_env[i + 1] = NULL;
+	j = 0;
+	while (j < i)
+	{
 		free((*env)[j]);
+		j++;
+	}
 	free(*env);
 	*env = new_env;
 }
@@ -99,15 +119,16 @@ int	export(char **argv, char ***en)
 {
 	int	i;
 
-	i = 0;
+	i = 1;
 	if (!argv[1])
 		return (env(*en));
-	while (argv[++i])
+	while (argv[i])
 	{
 		if (already_present(argv[i], *en) != -1)
 			update_exp(argv[i], en);
 		else
 			insert_exp(argv[i], en);
+		i++;
 	}
 	return (0);
 }
